76234a4fb06cbb161bce5cf5101e069cd81964f0
Victor Rosario Ishizuka <127458647+VictorIshizuka@users.noreply.github.com>
2026-03-03 18:38:52 -0300

    fix: correção de campo nome ao registrar usuario

--- views/auth/register.view.php
+++ views/auth/register.view.php
@@ -9,59 +9,76 @@
     </div>
 
     <!-- FORM -->
-    <form class="max-w-sm w-full space-y-6">
+    <div class="max-w-sm w-full space-y-6">
 
       <h2 class="text-2xl font-semibold text-white text-center lg:text-left">
         Criar conta
       </h2>
+      <form action="/register" method="post" class="max-w-sm w-full space-y-6">
 
-      <!-- Name -->
-      <div>
-        <label class="block text-sm mb-2 text-gray-300">
-          E-mail
-        </label>
-        <input type="text"
-          placeholder="Como você se chama?"
-          class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 focus:ring-1 focus:ring-lime-400 outline-none transition">
-      </div>
+        <!-- Name -->
+        <div>
+          <label class="block text-sm mb-2 text-gray-300">
+            Nome
+          </label>
+          <input type="text"
+            name="name" value="<?= old('name') ?? '' ?>"
+            placeholder="Como você se chama?"
+            class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 <?php if (isset($validations['name'])) { ?> focus: border-red-400 <?php } ?> focus:ring-1 focus:ring-lime-400 outline-none transition">
+          <?php if (isset($validations['name'])) { ?>
+            <div class="mt-1 text-xs text-error"><?= $validations['name'] ?></div>
+          <?php } ?>
+        </div>
 
-      <!-- Email -->
-      <div>
-        <label class="block text-sm mb-2 text-gray-300">
-          E-mail
-        </label>
-        <input type="email"
-          placeholder="Se e-mail aqui"
-          class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 focus:ring-1 focus:ring-lime-400 outline-none transition">
-      </div>
+        <!-- Email -->
+        <div>
+          <label class="block text-sm mb-2 text-gray-300">
+            E-mail
+          </label>
+          <input type="email"
+            name="email" value="<?= old('email') ?? '' ?>"
+            placeholder="Se e-mail aqui"
+            class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 <?php if (isset($validations['email'])) { ?> focus: border-red-400 <?php } ?> focus:ring-1 focus:ring-lime-400 outline-none transition">
+          <?php if (isset($validations['email'])) { ?>
+            <div class="mt-1 text-xs text-error"><?= $validations['email'] ?></div>
+          <?php } ?>
+        </div>
 
-      <!-- Senha -->
-      <div>
-        <label class="block text-sm mb-2 text-gray-300">
-          Senha
-        </label>
-        <input type="password"
-          placeholder="Escolha uma senha segura"
-          class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 focus:ring-1 focus:ring-lime-400 outline-none transition">
-      </div>
+        <!-- Senha -->
+        <div>
+          <label class="block text-sm mb-2 text-gray-300">
+            Senha
+          </label>
+          <input type="password"
+            name="password"
+            placeholder="Escolha uma senha segura"
+            class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 <?php if (isset($validations['password'])) { ?> focus: border-red-400 <?php } ?> focus:ring-1 focus:ring-lime-400 outline-none transition">
+          <?php if (isset($validations['password'])) { ?>
+            <div class="mt-1 text-xs text-error"><?= $validations['password'] ?></div>
+          <?php } ?>
+        </div>
 
-      <div>
-        <label class="block text-sm mb-2 text-gray-300">
-          Confirmar senha
-        </label>
-        <input type="password_confirmed"
-          placeholder="Repita sua senha para confirmar"
-          class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 focus:ring-1 focus:ring-lime-400 outline-none transition">
-      </div>
+        <div>
+          <label class="block text-sm mb-2 text-gray-300">
+            Confirmar senha
+          </label>
+          <input type="password"
+            name="password_confirmed"
+            placeholder="Repita sua senha para confirmar"
+            class="w-full px-4 py-3 rounded-lg bg-transparent text-white border border-gray-700 focus:border-lime-400 <?php if (isset($validations['password'])) { ?> focus: border-red-400 <?php } ?> focus:ring-1 focus:ring-lime-400 outline-none transition">
+          <?php if (isset($validations['password_confirmed'])) { ?>
+            <div class="mt-1 text-xs text-error"><?= $validations['password_confirmed'] ?></div>
+          <?php } ?>
+        </div>
 
-      <!-- Botão -->
-      <div class="pt-4 text-center lg:text-end">
-        <button
-          class="bg-lime-400 text-black font-medium px-6 py-3 rounded-xl w-full lg:w-auto transition hover:bg-lime-300">
-          Criar conta
-        </button>
-      </div>
-
+        <!-- Botão -->
+        <div class="pt-4 text-center lg:text-end">
+          <button
+            class="bg-lime-400 text-black font-medium px-6 py-3 rounded-xl w-full lg:w-auto transition hover:bg-lime-300">
+            Criar conta
+          </button>
+        </div>
+      </form>
       <!-- Fazer Login MOBILE + TABLET -->
       <div class="lg:hidden text-center text-sm text-gray-400 pt-4">
         Já possuo uma conta!
@@ -70,6 +87,6 @@
         </a>
       </div>
 
-    </form>
+    </div>
 
   </div>
